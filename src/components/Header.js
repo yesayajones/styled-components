2f5c45c697620f6c01d533b301a3bfc3db81f181
yesayajones <yesayajones@gmail.com>
2023-02-17 18:58:53 +0200

    feat: Passing props

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { StyledHeader } from './styles/Header.styled';
 
 export default function Header() {
 	return (
-		<StyledHeader>
+		<StyledHeader bg='red'>
 			<h1>Hubble</h1>
 		</StyledHeader>
 	);
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,14 +1,6 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
-	background-color: #ebfbff;
+	background-color: ${({ bg }) => bg};
 	padding: 40px 0;
-
-	h1 {
-		color: red;
-	}
-
-	&:hover {
-		background-color: black;
-	}
 `;
