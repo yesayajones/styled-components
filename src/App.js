85245dab14d3e419620280f6e603cdf77d0e127f
yesayajones <yesayajones@gmail.com>
2023-02-21 22:51:02 +0200

    Style landing page using style-components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import { ThemeProvider } from 'styled-components';
-import Header from './components/Header';
-//We use curry braces for exports that are not default
-import { Container } from './components/styles/Container.styled';
-import GlobalStyles from './components/styles/Global';
+import { ThemeProvider } from 'styled-components'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Card from './components/Card'
+import { Container } from './components/styles/Container.styled'
+import GlobalStyles from './components/styles/Global'
+import content from './content'
 
 const theme = {
-	colors: {
-		header: '#ebfbff',
-		body: '#fff',
-		footer: '#003333',
-	},
-};
-
-function App() {
-	return (
-		<ThemeProvider theme={theme}>
-			<>
-				<GlobalStyles />
-				<Header />
-				<Container>
-					<h1>Hello World</h1>
-				</Container>
-			</>
-		</ThemeProvider>
-	);
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+  mobile: '768px',
 }
 
-export default App;
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </Container>
+        <Footer />
+      </>
+    </ThemeProvider>
+  )
+}
+
+export default App
--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -1,30 +1,28 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css?family=Poppins:wght@300;400;600;700&display=swap');
-;
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap');
 
-* {
-  box-sizing: border-box;
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.body};
+    color: hsl(192, 100%, 9%);
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.15em;
+    margin: 0;
+  }
+
+  p {
+    opacity: 0.6;
+    line-height: 1.5;
+  }
+
+  img {
+    max-width: 100%;
 }
+`
 
-body {
-  background: #fff;
-  color: hsl(193, 100%, 9%);
-  font-family: 'Poppins', sans-serif;
-  font-size: 1.15em;
-  margin: 0;
-}
-
-p {
-  opacity: 0.6;
-  line-height: 1.5;
-}
-
-img {
-  max-width: 100%;
-}
-
-`;
-
-export default GlobalStyles;
+export default GlobalStyles
